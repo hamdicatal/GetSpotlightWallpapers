16ccf6a9401636eec25d566643a0f23a7a4e1468
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2018-12-27 13:27:08 +0300

    Add project files.

---
+++ Spotlight/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Spotlight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.lblBilgi = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAbout = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(12, 287);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(460, 61);
+            this.btnFetch.TabIndex = 1;
+            this.btnFetch.Text = "Spotlight Duvar Kağıtlarını Getir";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // lblBilgi
+            // 
+            this.lblBilgi.Location = new System.Drawing.Point(12, 208);
+            this.lblBilgi.Name = "lblBilgi";
+            this.lblBilgi.Size = new System.Drawing.Size(460, 70);
+            this.lblBilgi.TabIndex = 2;
+            this.lblBilgi.Text = resources.GetString("lblBilgi.Text");
+            this.lblBilgi.Click += new System.EventHandler(this.lblBilgi_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.Location = new System.Drawing.Point(401, 19);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(65, 13);
+            this.lblAbout.TabIndex = 4;
+            this.lblAbout.Text = "  Hakkında  ";
+            this.lblAbout.Click += new System.EventHandler(this.lblAbout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 360);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.lblBilgi);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFetch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get Spotlight Wallpapers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.Label lblBilgi;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAbout;
+    }
+}
+
---
+++ Spotlight/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotlight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string GetCurrentUserPath()
+        {
+            string path = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                path = Directory.GetParent(path).ToString();
+            }
+
+            return path;
+        }
+
+        string CreateFolder()
+        {
+            string path = GetCurrentUserPath();
+
+            // Specify a "currently active folder"
+            string activeDir = path + "//Desktop//";
+
+            DateTime time = DateTime.Now;
+
+            //Create a new subfolder under the current active folder
+            string folderName = "Spotlight-" + time.Hour;
+            string newPath = System.IO.Path.Combine(activeDir, folderName);
+
+            // Create the subfolder
+            System.IO.Directory.CreateDirectory(newPath);
+
+            // Create a new file name. This example generates
+            // a random string.
+            string newFileName = System.IO.Path.GetRandomFileName();
+
+            // Combine the new file name with the path
+            newPath = System.IO.Path.Combine(newPath, newFileName);
+
+            return folderName;
+        }
+
+        void Copy(string sourceDir, string targetDir)
+        {
+            Directory.CreateDirectory(targetDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
+
+            foreach (var directory in Directory.GetDirectories(sourceDir))
+                Copy(directory, Path.Combine(targetDir, Path.GetFileName(directory)));
+        }
+
+        void RenameThem()
+        {
+            string fPath = GetCurrentUserPath() + "\\Desktop\\" + CreateFolder() + "\\";
+            string fNewName = "spotlight";
+
+            string fExt;
+            string fFromName;
+            string fToName;
+            int i = 1;
+     
+            //copy all files from fPath to files array
+            FileInfo[] files = new DirectoryInfo(fPath).GetFiles();
+            //loop through all files
+            foreach (var f in files)
+            {
+                //get the filename without the extension
+                fFromName = Path.GetFileNameWithoutExtension(f.Name);
+                //get the file extension
+                fExt = Path.GetExtension(f.Name);
+ 
+                //set fFromName to the path + name of the existing file
+                fFromName = string.Format("{0}{1}", fPath, f.Name);
+                //set the fToName as path + new name + _i + file extension
+                fToName = string.Format("{0}{1}_{2}{3}.jpg", fPath, fNewName,i.ToString(), fExt);
+ 
+                //rename the file by moving to the same place and renaming
+                File.Move(fFromName, fToName);
+                //increment i
+                i++;
+            }
+        }
+
+        void DeleteOthers()
+        {
+            string fPath = GetCurrentUserPath() + "\\Desktop\\" + CreateFolder() + "\\";
+            string fExt;
+            string fFromName;
+            int i = 1;
+
+            //copy all files from fPath to files array
+            FileInfo[] files = new DirectoryInfo(fPath).GetFiles();
+            //loop through all files
+            foreach (var f in files)
+            {
+                //get the filename without the extension
+                fFromName = Path.GetFileNameWithoutExtension(f.Name);
+                //get the file extension
+                fExt = Path.GetExtension(f.Name);
+
+                Image newImage = Image.FromFile(fPath + f.Name);
+                Size eski = newImage.Size;
+
+                newImage.Dispose();
+
+                Size yeni = new Size(1920, 1080);
+
+                if (eski != yeni)
+                {
+                    if (File.Exists(fPath + f.Name))
+                    {
+                        File.Delete(fPath + f.Name);
+                    }
+                }
+                i++;
+            }
+        }
+        
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            string newPath = CreateFolder();
+            //CopyFiles(newPath);
+            string path = GetCurrentUserPath();
+            // Specify a "currently active folder"
+            string activeDir = path + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets";
+
+            string activeDir2 = path + "\\Desktop\\" + CreateFolder();
+
+            Copy(activeDir, activeDir2);
+
+            RenameThem();
+
+            DeleteOthers();
+        }
+
+        private void lblBilgi_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Windows'un sağladığı Spotlight duvar kağıtlarını .jpg formatında kaydetmeye yarayan araçtır. Windows 10 1607 sürümünde denenmiştir. Kullanmadan önce masaüstünde Spotlight isimli bir klasör olmadığından emin olun...\n\nHamdi Çatal - http://www.hamdicatal.com/", "Hakkında");
+        }
+    }
+}
